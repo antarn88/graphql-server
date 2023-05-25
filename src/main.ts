8dae3b6695bcd5d7feb9ab89770c35941e29f8da
Arnold <antaloczi.arnold@gmail.com>
2023-05-25 20:51:46 +0200

    Fix CORS, and add ID field

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ import { AppModule } from './app.module';
 dotenv.config();
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: true });
   await app.listen(process.env.PORT || 5000);
 }
 bootstrap();
--- src/posts/entities/post.entity.ts
+++ src/posts/entities/post.entity.ts
@@ -1,10 +1,13 @@
-import { Field, ObjectType } from '@nestjs/graphql';
+import { Field, ID, ObjectType } from '@nestjs/graphql';
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 import { Document } from 'mongoose';
 
 @ObjectType()
 @Schema()
 export class Post {
+  @Field(() => ID)
+  id: string;
+
   @Field(() => String)
   @Prop({ required: true })
   title: string;

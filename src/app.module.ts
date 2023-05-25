c47385aedfa8788338bb0464eb759f975f3d9b10
Arnold <antaloczi.arnold@gmail.com>
2023-05-25 15:19:20 +0200

    Set database connection and other settings

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,25 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import * as dotenv from 'dotenv';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
+dotenv.config();
+
 @Module({
-  imports: [],
+  imports: [
+    MongooseModule.forRoot(
+      [
+        'mongodb+srv://',
+        `${process.env.DB_USER}:`,
+        `${process.env.DB_PASSWORD}@`,
+        `${process.env.DB_HOST}/`,
+        `${process.env.DB_NAME}`,
+        '?retryWrites=true&w=majority',
+      ].join(''),
+    ),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })

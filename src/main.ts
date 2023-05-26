5f7c80b77ffcbe971bb6ac4bb739c7f357300149
Arnold <antaloczi.arnold@gmail.com>
2023-05-26 17:04:16 +0200

    Add morgan to project

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import * as dotenv from 'dotenv';
+import * as morgan from 'morgan';
 
 import { AppModule } from './app.module';
 
@@ -7,6 +8,9 @@ dotenv.config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
+
+  app.use(morgan(process.env.MORGAN_FORMAT || 'dev'));
+
   await app.listen(process.env.PORT || 5000);
 }
 bootstrap();
